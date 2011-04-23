22c761113b7a121c4ddb6f11f359f87d4fa3f08c
Juan C. Muller <juan@challengepost.com>
2011-04-23 13:02:37 -0400

    Initial commit. Doing nothing.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HeadlinesToHeadlines::Application.config.secret_token = 'e6bd3b80c12294edbc2704666c0d356669273475b69dc854c20cc9ba9a3a19c9c79453dcd045f159c9e249b5e62acc6e37a238a3fb4d526df04c36bc20380700'
