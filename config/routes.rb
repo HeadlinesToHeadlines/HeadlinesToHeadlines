ea5918a95914162216e778e2f56217893545e43c
Juan C. Muller <juan@challengepost.com>
2011-04-23 15:01:24 -0400

    Twitter log in.

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ HeadlinesToHeadlines::Application.routes.draw do
   end
 
   root :to => "rounds#index"
+  match "/auth/:provider/callback" => "sessions#create"  
+  match "/signout" => "sessions#destroy", :as => :signout  
+
 end
